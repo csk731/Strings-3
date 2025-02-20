c889a229df9277c124265ad3d16b3e2427b727b6
Chaitanya Allu <chaitanyaa1999@gmail.com>
2025-02-19 21:59:30 -0600

    Added Another Approach for LC273

--- LC273.java
+++ LC273.java
@@ -1,7 +1,9 @@
+// For Approach 1 & 2: Using Iteration & Recursion
 // Time Complexity : O(1)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 
+// Approach 1: Using Iteration
 public class LC273 {
     String[] unitArr1 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
 
@@ -33,6 +35,43 @@ public class LC273 {
             String x = helper(tempNum%1000);
             if(x.trim().length()>0){
                 ans =  x.trim() + " " + unitArr3[unitArr3Ptr] + " " + ans;
+            }
+            unitArr3Ptr++;
+            tempNum /= 1000;
+        }
+        return ans.trim();
+    }
+}
+
+
+// Approach 2: Using Recursion
+class LC273_2 {
+    String[] unitArr1 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+            "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+
+    String[] unitArr2 = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+    String[] unitArr3 = { "", "Thousand", "Million", "Billion" };
+
+    private String helper(int num) {
+        if (num == 0)
+            return "";
+        else if (num <= 19)
+            return unitArr1[num];
+        else if (num < 100)
+            return unitArr2[num / 10] + " " + helper(num % 10);
+        return unitArr1[num / 100] + " Hundred " + helper(num % 100);
+    }
+
+    public String numberToWords(int num) {
+        if (num == 0)
+            return "Zero";
+        int unitArr3Ptr = 0;
+        int tempNum = num;
+        String ans = "";
+        while (tempNum > 0) {
+            if (tempNum % 1000 != 0) {
+                ans = helper(tempNum % 1000).trim() + " " + unitArr3[unitArr3Ptr] + " " + ans;
             }
             unitArr3Ptr++;
             tempNum /= 1000;
